a56ee13af2d448acbe93645f8407f5abb9acc2dc
jp631 <47936264+jp631@users.noreply.github.com>
2020-08-27 18:42:04 -0400

    website under construction banner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+        <p id="first"> This site is currently under construction. </p>
+        <p id="second">Please come back another time.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Site currently under construction
+    </div>
+  );
+};
+
+export default App;
