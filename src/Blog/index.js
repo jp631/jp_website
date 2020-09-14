e49d8d89882afcc5a73d1324712375b878d715a7
jp631 <47936264+jp631@users.noreply.github.com>
2020-09-14 19:24:38 -0400

    to trigger aws

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -34,7 +34,7 @@ class Blog extends Component {
                     title: "The 7 and rainbow",
                     image: "",
                     date: "Jan 12, 2021",
-                    text: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
+                    text: "ok me is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
                 }
             ]
         }
@@ -65,7 +65,6 @@ class Blog extends Component {
   }
 
     render() {
-
  
         const {arrowPushMenu, pushMenuBlog} = this.state;
         return (
